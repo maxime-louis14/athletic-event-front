0fa7d7c14060dab662b6739dc8d8b0e6c3700699
Maxime <maximelouis321@gmail.com>
2022-02-08 20:42:17 +0100

    sd
    ss

--- js/vue.js
+++ js/vue.js
@@ -1,17 +1,18 @@
-new Vue({
-    el: "#app",
-    data() {
-      return {
-        Details: [ ],
-      }
-    },
-  
-    mounted() {
-      axios.get("http://vps-a47222b1.vps.ovh.net/TShirt")
-      .then((response) => {
-        this.Details = response.data
-      })
-    },
 
-  });
-  
+
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      Details: [ ],
+    }
+  },
+
+  mounted() {
+    axios.get("http://vps-a47222b1.vps.ovh.net/TShirt")
+    .then((response) => {
+      this.Details = response.data
+    })
+  },
+
+});
